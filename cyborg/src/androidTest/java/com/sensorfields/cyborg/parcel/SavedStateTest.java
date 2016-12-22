4abe0d0b0246d27fd5a177e9a736cafaf1bfebe1
Miko Väli <mikovali@gmail.com>
2016-12-23 00:51:07 +0200

    SavedState

--- cyborg/src/androidTest/java/com/sensorfields/cyborg/parcel/SavedStateTest.java
+++ cyborg/src/androidTest/java/com/sensorfields/cyborg/parcel/SavedStateTest.java
@@ -1,17 +1,77 @@
 package com.sensorfields.cyborg.parcel;
 
+import android.os.Parcel;
+import android.os.ParcelUuid;
+import android.os.Parcelable;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
-import static org.junit.Assert.assertTrue;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
 
 @RunWith(AndroidJUnit4.class)
 public class SavedStateTest {
 
+    private SavedState.StateAware stateAware;
+
+    @Before
+    public void setup() {
+        stateAware = mock(SavedState.StateAware.class);
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                Parcel state = invocation.getArgument(0);
+                state.writeInt(5);
+                state.writeString("testing");
+                state.writeLong(7L);
+                return null;
+            }
+        }).when(stateAware).onSaveState(isA(Parcel.class));
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                Parcel state = invocation.getArgument(0);
+                assertEquals(5, state.readInt());
+                assertEquals("testing", state.readString());
+                assertEquals(7L, state.readLong());
+                return null;
+            }
+        }).when(stateAware).onRestoreState(isA(Parcel.class));
+    }
+
     @Test
-    public void onSaveInstanceState() {
-        assertTrue(true);
+    public void parcelableSaveAndRestore() {
+        ParcelUuid superState = new ParcelUuid(UUID.randomUUID());
+
+        Parcelable savedState = SavedState.onSaveInstanceState(stateAware, superState);
+        Parcelable newSuperState = SavedState.onRestoreInstanceState(stateAware, savedState);
+
+        assertEquals(superState, newSuperState);
+    }
+
+    @Test
+    public void parcelableSaveAndRestoreThroughParcel() {
+        ParcelUuid superState = new ParcelUuid(UUID.randomUUID());
+
+        Parcelable savedState = SavedState.onSaveInstanceState(stateAware, superState);
+
+        Parcel parcel = Parcel.obtain();
+        parcel.writeParcelable(savedState, 0);
+        parcel.setDataPosition(0);
+        Parcelable newSavedState = parcel.readParcelable(getClass().getClassLoader());
+        parcel.recycle();
+
+        Parcelable newSuperState = SavedState.onRestoreInstanceState(stateAware, newSavedState);
+
+        assertEquals(superState, newSuperState);
     }
 }
--- cyborg/src/main/java/com/sensorfields/cyborg/parcel/SavedState.java
+++ cyborg/src/main/java/com/sensorfields/cyborg/parcel/SavedState.java
@@ -3,7 +3,7 @@ package com.sensorfields.cyborg.parcel;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public final class SavedState {
+public final class SavedState implements Parcelable {
 
     public interface StateAware {
         void onSaveState(Parcel state);
@@ -11,10 +11,60 @@ public final class SavedState {
     }
 
     public static Parcelable onSaveInstanceState(StateAware stateAware, Parcelable superState) {
-        return superState;
+        Parcel parcel = Parcel.obtain();
+        stateAware.onSaveState(parcel);
+        parcel.setDataPosition(0);
+        byte[] bytes = parcel.marshall();
+        parcel.recycle();
+        return new SavedState(superState, bytes);
     }
 
     public static Parcelable onRestoreInstanceState(StateAware stateAware, Parcelable state) {
-        return state;
+        SavedState savedState = (SavedState) state;
+        Parcel parcel = Parcel.obtain();
+        parcel.unmarshall(savedState.state, 0, savedState.state.length);
+        parcel.setDataPosition(0);
+        stateAware.onRestoreState(parcel);
+        parcel.recycle();
+        return savedState.superState;
     }
+
+    private final Parcelable superState;
+    private final byte[] state;
+
+    private SavedState(Parcelable superState, byte[] state) {
+        this.superState = superState;
+        this.state = state;
+    }
+
+    private SavedState(Parcel source, ClassLoader loader) {
+        superState = source.readParcelable(loader);
+        state = source.createByteArray();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeParcelable(superState, flags);
+        dest.writeByteArray(state);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<SavedState> CREATOR = new ClassLoaderCreator<SavedState>() {
+        @Override
+        public SavedState createFromParcel(Parcel source, ClassLoader loader) {
+            return new SavedState(source, loader);
+        }
+        @Override
+        public SavedState createFromParcel(Parcel source) {
+            return createFromParcel(source, null);
+        }
+        @Override
+        public SavedState[] newArray(int size) {
+            return new SavedState[size];
+        }
+    };
 }
