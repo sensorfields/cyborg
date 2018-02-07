26255f218c1b254e5b945781f6f8163436233a96
Miko Väli <mikovali@gmail.com>
2018-02-07 14:51:14 +0200

    Basic navigation example

--- sample/src/main/java/com/sensorfields/cyborg/sample/main/home/Processor.java
+++ sample/src/main/java/com/sensorfields/cyborg/sample/main/home/Processor.java
@@ -1,5 +1,8 @@
 package com.sensorfields.cyborg.sample.main.home;
 
+import com.sensorfields.cyborg.navigator.Navigator;
+import com.sensorfields.cyborg.sample.beer.list.BeerListScreen;
+
 import javax.inject.Inject;
 
 import io.reactivex.Observable;
@@ -8,11 +11,26 @@ import io.reactivex.ObservableTransformer;
 
 final class Processor implements ObservableTransformer<Action, Result> {
 
-    @Inject Processor() {
+    private final ObservableTransformer<Action.RenderAction, Result.RenderResult> render;
+
+    private final ObservableTransformer<Action.BeerListAction, Result.RenderResult> beerList;
+
+    @Inject Processor(Navigator navigator) {
+        render = upstream -> upstream.map(action -> Result.RenderResult.create());
+
+        beerList = upstream -> upstream
+                .filter(action -> {
+                    navigator.push(BeerListScreen.create());
+                    return false;
+                })
+                .map(action -> Result.RenderResult.create());
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public ObservableSource<Result> apply(Observable<Action> upstream) {
-        return upstream.map(action -> Result.RenderResult.create());
+        return upstream.publish(shared -> Observable.mergeArray(
+                shared.ofType(Action.RenderAction.class).compose(render),
+                shared.ofType(Action.BeerListAction.class).compose(beerList)));
     }
 }
